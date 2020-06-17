4f69a2b0d7640d4aa68a0cefbf22aefea57db76a
Bhanu Singh <bhanu@krenovate.com>
2020-06-17 14:08:04 +0530

    Get Order Data

--- crush-product-program-section.php
+++ crush-product-program-section.php
@@ -15,15 +15,36 @@ if(!defined('ABSPATH')){
 	exit;
 }
 
-require_once(plugin_dir_path(__FILE__).'includes/crush-product-program-section-scripts.php');
-
-require_once(plugin_dir_path(__FILE__).'shortcodes/crush-personal-products-data.php');
-
-require_once(plugin_dir_path(__FILE__).'includes/personal-fitness-plan-class.php');
+add_action('plugins_loaded', 'my_plugin_init');
 
 
+function my_plugin_init() {
 
-$class = new PersonalFitnessClass();
+	if ( class_exists('WC_Order_Query') ) {
+		// Put your plugin code here
+		require_once(plugin_dir_path(__FILE__).'includes/crush-product-program-section-scripts.php');
 
+		require_once(plugin_dir_path(__FILE__).'shortcodes/crush-personal-products-data.php');
+
+		add_action( 'woocommerce_after_register_post_type', 'crush_get_order_data' );
+
+
+	} else {
+		add_action('admin_notices', 'wc_not_loaded');
+	}
+}
+
+function wc_not_loaded() {
+	printf(
+		'<div class="error"><p>%s</p></div>',
+		__('Sorry cannot create coupon because WooCommerce is not loaded')
+	);
+}
+
+
+function crush_get_order_data(){
+	require_once(plugin_dir_path(__FILE__).'includes/personal-fitness-plan-class.php');
+	$class = new PersonalFitnessClass();
+}
 
 
--- includes/personal-fitness-plan-class.php
+++ includes/personal-fitness-plan-class.php
@@ -2,44 +2,30 @@
 
 class PersonalFitnessClass {
 
-
-
-
-
 	public function __construct(){
 
-		$query = new \WC_Order_Query(array(
-			'limit' => - 1,
+		$query = new WC_Order_Query( array(
+			'limit' => -1,
 			'orderby' => 'date',
 			'order' => 'DESC',
-			'customer_id' => get_current_user_id() ,
-			'status' => array(
-				'completed',
-				'processing'
-			) ,
-		));
+			'return' => 'ids',
+			'customer_id' => get_current_user_id(),
+		) );
 
-		if ($query) {
+		$orders = $query->get_orders();
 
-			$orders = $query->get_orders();
+		foreach($orders as $order_id ) {
+			// Get an instance of the WC_Order object
+			$order = wc_get_order($order_id);
 
-			if (!empty($orders))
-			{
-				foreach ($orders as $order)
-				{
-					$items = $order->get_items();
-					if (!empty($items))
-					{
-						foreach ($items as $item)
-						{
-							$product_id = $item->get_product_id();
-							$term_list = wp_get_post_terms($product_id,'product_cat',array('fields'=>'ids'));
-							var_dump($item);
-						}
-					}
-				}
-			}
+			// Iterating through each WC_Order_Item_Product objects
+			foreach ($order->get_items() as $item_key => $item ):
+
+				$product_id   = $item->get_product_id(); // the Product id
+				$item_name    = $item->get_name(); // Name of the product
+
+				var_dump($product_id, $item_name);
+			endforeach;
 		}
-
 	}
 }
